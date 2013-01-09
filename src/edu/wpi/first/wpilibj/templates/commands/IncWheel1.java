de5a1c4030ce09afcd106aa66e74fd28f09ca777
Chris <chris@lentner.me>
2013-01-09 17:06:05 -0500

    remove unused imports

--- src/edu/wpi/first/wpilibj/templates/commands/IncWheel1.java
+++ src/edu/wpi/first/wpilibj/templates/commands/IncWheel1.java
@@ -1,7 +1,5 @@
 
 package edu.wpi.first.wpilibj.templates.commands;
-import edu.wpi.first.wpilibj.templates.subsystems.*;
-
 public class IncWheel1 extends CommandBase {
     
     public IncWheel1() {
